19439448a6c6e8198baae1123cba5f2043a2754e
Nikhil-4102 <nikhilmalave04ofcl@gmail.com>
2025-04-28 18:38:54 +0530

    get API done from local Json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UsersService } from './service/users.service';
+import { User } from './interfaces/User';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Angular-CRUD_Oprations_API';
+
+  users: User[] = [];
+  constructor(private userService:UsersService) { 
+
+  }
+  ngOnInit(){
+     this.userService.getUsers().subscribe((data:User[]) => {
+      this.users = data;
+      console.log(this.users);
+     })
+  }
 }
